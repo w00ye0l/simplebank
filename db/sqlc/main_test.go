a6b435ed9434b3895110df3a29726c5d1272047d
w00ye0l <dnduf158@gmail.com>
2025-09-23 20:46:31 +0900

    fix: map postgres port

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	dbDriver = "postgres"
-	dbSource = "postgres://root:secret@localhost:5433/simple_bank?sslmode=disable"
+	dbSource = "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
